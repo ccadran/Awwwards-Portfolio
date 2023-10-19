7cca330aed121d4fba431951818d4ec281b374b0
ccadran <clariocadran33@gmail.com>
2023-10-19 22:09:09 +0200

    add Landing components

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Landing from "../components/landing";
 
 export default function Home() {
-  return <main className={styles.main}></main>;
+  return (
+    <main className={styles.main}>
+      <Landing />
+    </main>
+  );
 }
---
+++ src/components/landing/index.jsx
@@ -0,0 +1,53 @@
+"use client";
+import Image from "next/image";
+import styles from "./style.module.scss";
+import gsap from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+export default function Home() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = 1;
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    requestAnimationFrame(animation);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+        scrub: 0.25,
+      },
+      x: "-300px",
+    });
+  }, []);
+  const animation = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    }
+    if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animation);
+    xPercent += 0.03 * direction;
+  };
+
+  return (
+    <main className={styles.main}>
+      <Image fill={true} src="/images/background.jpg" />
+      <div className={styles.sliderContainer}>
+        <div ref={slider} className={styles.slider}>
+          <p ref={firstText}>Freelance Photographer -</p>
+          <p ref={secondText}>Freelance Photographer -</p>
+        </div>
+      </div>
+    </main>
+  );
+}
