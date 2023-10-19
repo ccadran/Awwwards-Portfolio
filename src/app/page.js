9b96b0a0b8ab5cbb75734dd467522f857da30cd5
ccadran <clariocadran33@gmail.com>
2023-10-19 22:54:32 +0200

    add projects gallery

--- src/app/page.js
+++ src/app/page.js
@@ -2,12 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import Landing from "../components/landing";
 import Description from "../components/description";
+import Projects from "../components/projects";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Landing />
       <Description />
+      <Projects />
     </main>
   );
 }
