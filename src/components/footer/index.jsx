261c64f9e251d332ba23ff24357a4bb9a134f478
ccadran <clariocadran33@gmail.com>
2023-10-21 10:34:08 +0200

    add anim on footer elemente

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import styles from "./style.module.scss";
 import Image from "next/image";
+import { useTransform, useScroll, motion } from "framer-motion";
 
 export default function index() {
+  const container = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: container,
+    offset: ["start end", "end end"],
+  });
+
+  const x = useTransform(scrollYProgress, [0, 1], [0, 100]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [120, 90]);
+  const y = useTransform(scrollYProgress, [0, 1], [-500, 0]);
+
   return (
-    <div className={styles.contact}>
+    <motion.div style={{ y }} ref={container} className={styles.contact}>
       <div className={styles.body}>
         <div className={styles.title}>
           <span>
@@ -14,12 +26,13 @@ export default function index() {
             <h2>Let's work</h2>
           </span>
           <h2>together</h2>
-          <div className={styles.buttonContainer}>
+          <motion.div style={{ x }} className={styles.buttonContainer}>
             <div backgroundColor={"#334BD3"} className={styles.button}>
               <p>Get in touch</p>
             </div>
-          </div>
-          <svg
+          </motion.div>
+          <motion.svg
+            style={{ rotate, scale: 2 }}
             width="9"
             height="9"
             viewBox="0 0 9 9"
@@ -30,7 +43,7 @@ export default function index() {
               d="M8 8.5C8.27614 8.5 8.5 8.27614 8.5 8L8.5 3.5C8.5 3.22386 8.27614 3 8 3C7.72386 3 7.5 3.22386 7.5 3.5V7.5H3.5C3.22386 7.5 3 7.72386 3 8C3 8.27614 3.22386 8.5 3.5 8.5L8 8.5ZM0.646447 1.35355L7.64645 8.35355L8.35355 7.64645L1.35355 0.646447L0.646447 1.35355Z"
               fill="white"
             />
-          </svg>
+          </motion.svg>
         </div>
         <div className={styles.nav}>
           <p>info@dennissnellenberg.com</p>
@@ -63,6 +76,6 @@ export default function index() {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
