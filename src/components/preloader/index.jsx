75cd61d58571c00276a148ff9db4436afaadb745
ccadran <clariocadran33@gmail.com>
2023-10-23 10:33:32 +0200

    add style to the loading component

--- src/components/preloader/index.jsx
+++ src/components/preloader/index.jsx
@@ -1,4 +1,8 @@
 import styles from "./style.module.scss";
 export default function index() {
-  return <div>index</div>;
+  return (
+    <div className={styles.introduction}>
+      <p>Is loading</p>
+    </div>
+  );
 }
