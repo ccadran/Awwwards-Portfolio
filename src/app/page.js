eea4babe4a5b496f930b3fd9c94eb74867456c7a
ccadran <clariocadran33@gmail.com>
2023-10-23 10:30:18 +0200

    add loading components

--- src/app/page.js
+++ src/app/page.js
@@ -6,17 +6,24 @@ import Description from "../components/description";
 import Projects from "../components/projects";
 import SlidingImage from "../components/slidingImage";
 import Footer from "../components/footer";
-import { useEffect } from "react";
+import Preloader from "../components/preloader";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     (async () => {
       const LocomotiveScroll = (await import("locomotive-scroll")).default;
       const locomotiveScroll = new LocomotiveScroll();
+      setTimeout(() => {
+        setIsLoading(false);
+        document.body.style.cursor = "default";
+      }, 2000);
     })();
   }, []);
   return (
     <main className={styles.main}>
+      {isLoading && <Preloader />}
       <Landing />
       <Description />
       <Projects />
---
+++ src/components/preloader/index.jsx
@@ -0,0 +1,4 @@
+import styles from "./style.module.scss";
+export default function index() {
+  return <div>index</div>;
+}
